d637ec04130d4724533f52c084d5e477259dc428
RadoslawPiorkowski <piorkowski.rad@gmail.com>
2020-08-27 15:12:50 +0200

    contact

--- src/main/java/com/pokemonCardRouletteWeb/Controllers/KontakController.java
+++ src/main/java/com/pokemonCardRouletteWeb/Controllers/KontakController.java
@@ -1,14 +1,41 @@
 package com.pokemonCardRouletteWeb.Controllers;
 
+import java.sql.SQLException;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.pokemonCardRouletteWeb.BazaDanych;
+import com.pokemonCardRouletteWeb.Data.Wiadomosc;
 
 @Controller
 public class KontakController {
 
 	
     @GetMapping("/kontakt")
-    public String stronaKontaktu() {
+    public String stronaKontaktu(Model model) {
+    	model.addAttribute("wiadomosc", new Wiadomosc());
         return "kontakt";
+    }
+    
+    
+    @PostMapping("/kontakt")
+    public String getWiadomosc(@ModelAttribute Wiadomosc wiadomosc) {
+    	
+		if(!wiadomosc.getName().isEmpty() && !wiadomosc.getMail().isEmpty() && !wiadomosc.getSubject().isEmpty() && !wiadomosc.getText().isEmpty() && !wiadomosc.getNumber().isEmpty()) {
+			Wiadomosc.listaWiadomosci.add(wiadomosc);
+			 try {
+				 BazaDanych.getStatmentBD().execute("INSERT INTO wiadomosc (imie, mail, numer, temat, tekst) VALUES "+ "('" + wiadomosc.getName() + "' , '" + wiadomosc.getMail() + "' , '" + wiadomosc.getNumber() + "' , '" + wiadomosc.getSubject() + "' , '" + wiadomosc.getText() + "');");
+             } catch (SQLException e) {
+                 e.printStackTrace();
+             }
+		
+			return "wyslanoWiadomosc";
+		}
+		
+		return "home";
     }
 }
