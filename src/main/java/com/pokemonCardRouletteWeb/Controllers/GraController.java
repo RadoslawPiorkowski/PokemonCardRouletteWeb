adcd7aaa83664e72640273523d194c226c600959
RadoslawPiorkowski <piorkowski.rad@gmail.com>
2020-08-27 21:40:27 +0200

    contact

--- src/main/java/com/pokemonCardRouletteWeb/Controllers/GraController.java
+++ src/main/java/com/pokemonCardRouletteWeb/Controllers/GraController.java
@@ -1,10 +1,8 @@
 package com.pokemonCardRouletteWeb.Controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.pokemonCardRouletteWeb.Data.Gracz;
 import com.pokemonCardRouletteWeb.Data.GraczZalogowany;
 
 
@@ -14,13 +12,12 @@ public class GraController {
 	
 	
     @GetMapping("/gra")
-    public String stronaGry(Model model) {
-    	model.addAttribute("gracz", new Gracz());
+    public String stronaGry() {
     	
     	if(GraczZalogowany.nick.equals(""))
     		return "redirect:/logowanie";
     	
-        return "logowanie";
+        return "gra";
     }
 	
 }
