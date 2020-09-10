c106c9053f19b7db964e02f9bf11cfb96c9c8606
gniewkoF <maciej.wojteczek@gmail.com>
2020-09-10 06:18:45 +0200

    message

--- src/main/java/com/pokemonCardRouletteWeb/BazaDanych.java
+++ src/main/java/com/pokemonCardRouletteWeb/BazaDanych.java
@@ -6,7 +6,7 @@ public class BazaDanych {
 	
 	public static String urlBazy = "jdbc:postgresql:pokemon";
 	public static String uzytkownik = "postgres";
-	public static String hasloBazyDanych = "425@hejBudowa";
+	public static String hasloBazyDanych = "gniewko12";
 	
 	
 	public static Statement getStatmentBD() throws SQLException{
